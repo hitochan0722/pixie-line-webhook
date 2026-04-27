6cb2f018d5bce3a7579baf5ab057c9eb1afc2906
hitochan0722 <missdebbie1987@gmail.com>
2026-04-27 17:21:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ def board():
     return render_template("board.html")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
